9cb5401e16b34d628a39da83bf1856e9b8fc92b4
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2022-07-03 11:05:56 -0400

    cartaopop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch,  Route, Link, Routes, Router, BrowserRouter } from 'react-router-dom';
+import React from 'react';
+
+import Pageprinc from "./pages/pagePrinc";
+import Pageusuario from './pages/pageUsuario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Pageprinc/>}/>
+        <Route exact path="/usuario" element={<Pageusuario/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
